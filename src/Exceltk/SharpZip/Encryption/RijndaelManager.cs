84ba37868f80f1062ea540076298c1531c969deb
fanfeilong <fanfeilong@xunlei.com>
2016-11-09 09:02:34 +0800

    ignore temp dirs

--- src/Exceltk/SharpZip/Encryption/RijndaelManager.cs
+++ src/Exceltk/SharpZip/Encryption/RijndaelManager.cs
@@ -5,15 +5,14 @@
 using System;
 using System.Security.Cryptography;
 
-namespace ICSharpCode.SharpZipLib.Encryption
-{
-    public static class Extension{
-       public static KeySizes[] CloneKeySizesArray(this KeySizes[] src){
-           return (KeySizes[])(src.Clone());
-       }
+namespace ICSharpCode.SharpZipLib.Encryption {
+    public static class Extension {
+        public static KeySizes[] CloneKeySizesArray(this KeySizes[] src) {
+            return (KeySizes[])(src.Clone());
+        }
     }
     public abstract class Rijndael2 : SymmetricAlgorithm {
-        public static Rijndael2 Create() {
+        public new static Rijndael2 Create() {
             return new RijndaelImplementation();
         }
 
@@ -41,94 +40,105 @@ namespace ICSharpCode.SharpZipLib.Encryption
     /// be consistent with the rest of the static Create() methods which return opaque types.
     /// They both have have the same implementation.
     /// </summary>
-    internal sealed class RijndaelImplementation : Rijndael2
-    {
+    internal sealed class RijndaelImplementation : Rijndael2 {
         private readonly Aes _impl;
 
-        internal RijndaelImplementation()
-        {
-            LegalBlockSizesValue = new KeySizes[] { new KeySizes(minSize: 128, maxSize: 128, skipSize: 0) };
+        internal RijndaelImplementation() {
+            LegalBlockSizesValue=new KeySizes[] { new KeySizes(minSize: 128, maxSize: 128, skipSize: 0) };
 
             // This class wraps Aes
-            _impl = Aes.Create();
+            _impl=Aes.Create();
         }
 
-        public override int BlockSize
-        {
-            get { return _impl.BlockSize; }
-            set
-            {
+        public override int BlockSize {
+            get {
+                return _impl.BlockSize;
+            }
+            set {
                 // Values which were legal in desktop RijndaelManaged but not here in this wrapper type
-                if (value == 192 || value == 256)
+                if (value==192||value==256)
                     throw new PlatformNotSupportedException("Cryptography_Rijndael_BlockSize Can NOT be 192 or 256");
 
                 // Any other invalid block size will get the normal “invalid block size” exception.
-                _impl.BlockSize = value;
+                _impl.BlockSize=value;
             }
         }
 
-        public override byte[] IV
-        {
-            get { return _impl.IV; }
-            set { _impl.IV = value; }
+        public override byte[] IV {
+            get {
+                return _impl.IV;
+            }
+            set {
+                _impl.IV=value;
+            }
         }
 
-        public override byte[] Key
-        {
-            get { return _impl.Key; }
-            set { _impl.Key = value; }
+        public override byte[] Key {
+            get {
+                return _impl.Key;
+            }
+            set {
+                _impl.Key=value;
+            }
         }
 
-        public override int KeySize
-        {
-            get { return _impl.KeySize; }
-            set { _impl.KeySize = value; }
+        public override int KeySize {
+            get {
+                return _impl.KeySize;
+            }
+            set {
+                _impl.KeySize=value;
+            }
         }
-        public override CipherMode Mode
-        {
-            get { return _impl.Mode; }
-            set { _impl.Mode = value; }
+        public override CipherMode Mode {
+            get {
+                return _impl.Mode;
+            }
+            set {
+                _impl.Mode=value;
+            }
         }
 
-        public override PaddingMode Padding
-        {
-            get { return _impl.Padding; }
-            set { _impl.Padding = value; }
+        public override PaddingMode Padding {
+            get {
+                return _impl.Padding;
+            }
+            set {
+                _impl.Padding=value;
+            }
         }
 
-        public override KeySizes[] LegalKeySizes{
-            get{
+        public override KeySizes[] LegalKeySizes {
+            get {
                 return _impl.LegalKeySizes;
             }
         }
-        public override ICryptoTransform CreateEncryptor(){
-                return _impl.CreateEncryptor();
-        } 
-        public override ICryptoTransform CreateEncryptor(byte[] rgbKey, byte[] rgbIV){
-            return   _impl.CreateEncryptor(rgbKey, rgbIV);
+        public override ICryptoTransform CreateEncryptor() {
+            return _impl.CreateEncryptor();
         }
-        public override ICryptoTransform CreateDecryptor(){
-            return _impl.CreateDecryptor();  
-        } 
-        public override ICryptoTransform CreateDecryptor(byte[] rgbKey, byte[] rgbIV){
-          return _impl.CreateDecryptor(rgbKey, rgbIV);  
-        } 
-        public override void GenerateIV(){
-            _impl.GenerateIV();   
+        public override ICryptoTransform CreateEncryptor(byte[] rgbKey, byte[] rgbIV) {
+            return _impl.CreateEncryptor(rgbKey, rgbIV);
         }
-        public override void GenerateKey(){
+        public override ICryptoTransform CreateDecryptor() {
+            return _impl.CreateDecryptor();
+        }
+        public override ICryptoTransform CreateDecryptor(byte[] rgbKey, byte[] rgbIV) {
+            return _impl.CreateDecryptor(rgbKey, rgbIV);
+        }
+        public override void GenerateIV() {
+            _impl.GenerateIV();
+        }
+        public override void GenerateKey() {
             _impl.GenerateKey();
-        } 
+        }
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-            #if !OS_WINDOWS
+        protected override void Dispose(bool disposing) {
+            if (disposing) {
+#if !OS_WINDOWS
                 _impl.Dispose();
-            #else
+#else
                 _impl.Clear();
-            #endif
+#endif
             }
         }
     }
