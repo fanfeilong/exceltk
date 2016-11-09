18eb98026c19d35c05f20d28dd810a3072118332
feilong <fanfeilong@outlook.com>
2016-11-09 16:30:51 +0800

    pass testcase in macos

--- src/test.sh
+++ src/test.sh
@@ -1,8 +1,9 @@
-dotnet run -t md -xls test\test1.xlsx
-dotnet run -t md -xls test\test2.xlsx
-dotnet run -t md -xls test\test3.xlsx
-dotnet run -t md -xls test\test4.xlsx
-dotnet run -t md -xls test\test5.xlsx
-dotnet run -t md -xls test\test6_crossline.xlsx
-dotnet run -t md -xls test\test7_large_tail_empty_columns.xlsx
-dotnet run -t md -xls test\test8.xls
+#!/bin/bash
+dotnet run -t md -xls test/test1.xlsx
+dotnet run -t md -xls test/test2.xlsx
+dotnet run -t md -xls test/test3.xlsx
+dotnet run -t md -xls test/test4.xlsx
+dotnet run -t md -xls test/test5.xlsx
+dotnet run -t md -xls test/test6_crossline.xlsx
+dotnet run -t md -xls test/test7_large_tail_empty_columns.xlsx
+dotnet run -t md -xls test/test8.xls
